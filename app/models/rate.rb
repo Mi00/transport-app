7eca49c2cdac55bc98891cce3979192820273175
Mi00 <muturuzi@gmail.com>
2017-05-28 16:23:20 +0200

    added PLN validation

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -4,13 +4,25 @@ class Rate < ApplicationRecord
 	  before_save :get_values
 
 	  def get_values
-	    url = "http://api.nbp.pl/api/exchangerates/rates/a/#{currency.downcase}/#{date}/?format=json"
-	    response = HTTParty.get(url)
-	    data = JSON.parse(response.body)
-	    self.table = data['rates'][0]['no']	    
-	    self.table_date = data['rates'][0]['effectiveDate']	    
-	    self.converter = data['rates'][0]['mid']
-	    value_pln = value_currency * converter
-	    self.value_pln = value_pln.round(2)
+	  	if currency != "PLN"
+		  	url = "http://api.nbp.pl/api/exchangerates/rates/a/#{currency.downcase}/#{date}/?format=json"
+		    response = HTTParty.get(url)
+
+		  	until response.body != "404 NotFound - Not Found - Brak danych" do
+		  		self.date = date.yesterday
+			    url = "http://api.nbp.pl/api/exchangerates/rates/a/#{currency.downcase}/#{date}/?format=json"
+			    response = HTTParty.get(url)		    
+			end
+			
+			data = JSON.parse(response.body)
+		    self.table = data['rates'][0]['no']	    
+		    self.table_date = data['rates'][0]['effectiveDate']	    
+		    self.converter = data['rates'][0]['mid']
+		    value_pln = value_currency * converter
+		    self.value_pln = value_pln.round(2)
+
+		else
+			self.value_pln = value_currency
+		end
   	  end
 end
