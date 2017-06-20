6f3abf7ddb3f085c2405906cbeffb9746c996380
Mi00 <muturuzi@gmail.com>
2017-06-20 09:12:22 +0200

    statistic search by dates

--- app/controllers/statistic_controller.rb
+++ app/controllers/statistic_controller.rb
@@ -9,6 +9,11 @@ class StatisticController < ApplicationController
 		end
 =end
 		@cars = Car.all
-		# @rates = Rate.where("created_at >= :start_date AND created_at <= :end_date", {start_date:  params[:start_date], end_date: params[:end_date]})
+		if params[:start_date] != nil 
+			@rates = Rate.where("date >= :start_date AND date <= :end_date", {start_date:  params[:start_date], end_date: params[:end_date]})
+			@sum_rates = @rates.sum(:value_pln)
+		else
+			@rates = nil
+		end
 	end
 end
--- app/helpers/statistic_helper.rb
+++ app/helpers/statistic_helper.rb
@@ -1,15 +1,7 @@
 module StatisticHelper
 	def sum(car)
-		if car != nil
-			Car.find(car).rates.sum("value_pln")
+		if car != nil && @rates != nil
+      @rates.where(car_id: "#{car}").sum(:value_pln)
 		end
 	end
-
-  def sum_month(car, date)
-    if date != nil
-      month = date['month']
-      year = date['year']
-      rates = Car.find(car).rates.where("cast(strftime('%m', date_column) as int) = ?", month)
-    end
-  end
 end
